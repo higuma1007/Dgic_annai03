3a6e18c6c30276de49d8e9de528eb6624cf55ae0
higuma-san <mi-obara@dgic.co.jp>
2023-02-07 13:40:52 +0900

    コメント位置を合わせる

--- js/loadYoutube.js
+++ js/loadYoutube.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-//ローディング画面
+// ローディング画面
 $(window).on('load', function () {
     $("#youtube-area").addClass('appear');
     $("#loading").addClass('disappear');
@@ -12,19 +12,19 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var ytPlayer;
 function onYouTubeIframeAPIReady() {
-    ytPlayer = new YT.Player('youtube', {//動画を表示させたいIDを指定
-        videoId: 'ZXvguPK-T6E',//動画のアドレスの指定
+    ytPlayer = new YT.Player('youtube', {   // 動画を表示させたいIDを指定
+        videoId: 'ZXvguPK-T6E',             // 動画のアドレスの指定
         playerVars: {
-            playsinline: 1,// インライン再生を行う
-            autoplay: 1,//自動再生を行う
-            fs: 0,//全画面表示ボタンを表示しない    
-            rel: 0,// 再生中の動画と同じチャンネルの関連動画を表示
-            controls: 0,// プレーヤー コントロールを表示しない
-            modestbranding: 1, // YouTubeロゴの非表示
-            iv_load_policy: 3, // アノテーションの非表示
-            start: 0,//0秒後から動画がスタート
+            playsinline: 1,                 // インライン再生を行う
+            autoplay: 1,                    // 自動再生を行う
+            fs: 0,                          // 全画面表示ボタンを表示しない    
+            rel: 0,                         // 再生中の動画と同じチャンネルの関連動画を表示
+            controls: 0,                    // プレーヤー コントロールを表示しない
+            modestbranding: 1,              // YouTubeロゴの非表示
+            iv_load_policy: 3,              // アノテーションの非表示
+            start: 0,                       // 0秒後から動画がスタート
         },
-        events: {//　イベント
+        events: {                           //　イベント
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
         }
